210622f601cb17808214f1b17ae436f9f3405294
Jon <jonathan.verrier@hotmail.com>
2024-09-22 23:08:22 +0100

    Added specs for Chunk and FindTheme APIs

--- BraidApi/BraidCommon/src/ThemeApi.js
+++ BraidApi/BraidCommon/src/ThemeApi.js
@@ -1,5 +1,5 @@
 "use strict";
 // Copyright (c) 2024 Braid Technologies Ltd
-// Definitions for the data elements of the Chunk API
+// Definitions for the data elements of the FindTheme API
 Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=Chunk.js.map
+//# sourceMappingURL=ThemeApi.js.map
