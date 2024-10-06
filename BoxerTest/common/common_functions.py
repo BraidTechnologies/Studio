b4380f009caf8c21d60c470ca164a999b8e5740e
prathamkuril <prathamkuril@gmail.com>
2024-10-06 12:04:36 +0100

    Update common_functions.py

--- BoxerTest/common/common_functions.py
+++ BoxerTest/common/common_functions.py
@@ -41,9 +41,3 @@ def get_embedding(text: str, client: AzureOpenAI, config: ApiConfiguration, mode
     )
     
     return response.data[0].embedding
-
-
-
-def get_embedding(text, model="text-embedding-3-small"):
-   text = text.replace("\n", " ")
-   return client.embeddings.create(input = [text], model=model).data[0].embedding
