49442d224b6e51bd4652737278cc8c6b94985eed
Jon <jonathan.verrier@hotmail.com>
2024-12-16 19:12:58 +0000

    Add React.StrictMode, add reportwebVitals - nees a look at Google Analytics to understand.

--- WaterfallBrowser/src/App.tsx
+++ WaterfallBrowser/src/App.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { FluentProvider, teamsDarkTheme, makeStyles } from '@fluentui/react-components';
 import ChunkRetriever from './ChunkRetriever';
 import { retrieveChunk } from './ChunkRetriever';
+import reportWebVitals from './reportWebVitals';
 
 const fluidFillPageStyles = makeStyles({
    root: {
@@ -76,6 +77,14 @@ export default App;
 if (document !== undefined && document.getElementById !== undefined) {
    const root = createRoot(document.getElementById("reactRoot") as HTMLElement);
    root.render(
-      <App />
+      <React.StrictMode>
+         <App />
+      </React.StrictMode>      
    ); 
 }
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
