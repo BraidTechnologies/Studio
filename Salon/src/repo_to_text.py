bd8a8c4dbd5082791d50734a8f6cc1e85fea2d7b
pratham-kuril <pratham@braidtech.ai>
2025-02-18 13:27:25 +0000

    fall back files

--- Salon/src/repo_to_text.py
+++ Salon/src/repo_to_text.py
@@ -21,11 +21,11 @@ Options:
 Example:
     python src/repo_to_text.py --cfg config.yaml --repo_path . -o test_output
     python src/repo_to_text.py --cfg config.yaml --repo_path ./my_repo -w 100000 -o ./output --skip_patterns "*.md" "*.txt" --skip_dirs "tests" -v
-    python "C:\\BraidTechnologies\\Studio\\Salon\src\\repo_to_text.py" --cfg "D:\Braid Technologies\Fork_January2025_repo\WorkedExamples\Salon\config.yaml" --repo_path . -o test_output
-    python "C:\\BraidTechnologies\\Studio\Salon\\src\\repo_to_text.py" --cfg "D:\Braid Technologies\Fork_January2025_repo\WorkedExamples\Salon\config.yaml" --repo_path . -o test_output
-    python "C:\\BraidTechnologies\\Studio\Salon\\src\\repo_to_text.py" --cfg config.yaml --repo_path . -o test_output
-    python "C:\\BraidTechnologies\\Studio\Salon\\src\\repo_to_text.py" --cfg "C:\\BraidTechnologies\\Studio\\Salon\\config.yaml" --repo_path "C:\\BraidTechnologies\\Studio\\Salon" -o test_output
-    python "C:\\BraidTechnologies\\Studio\Salon\\src\\repo_to_text_old.py" --cfg config.yaml --repo_path . -o test_output
+    python "C:\\BraidTechnologies\\Repo\\Studio\\Salon\src\\repo_to_text.py" --cfg "D:\Braid Technologies\Fork_January2025_repo\WorkedExamples\Salon\config.yaml" --repo_path . -o test_output
+    python "C:\\BraidTechnologies\\Repo\\Studio\Salon\\src\\repo_to_text.py" --cfg "D:\Braid Technologies\Fork_January2025_repo\WorkedExamples\Salon\config.yaml" --repo_path . -o test_output
+    python "C:\\BraidTechnologies\\Repo\\Studio\Salon\\src\\repo_to_text.py" --cfg config.yaml --repo_path . -o test_output
+    python "C:\\BraidTechnologies\\Repo\\Studio\Salon\\src\\repo_to_text.py" --cfg "C:\\BraidTechnologies\\Repo\\Studio\\Salon\\config.yaml" --repo_path "C:\\BraidTechnologies\\Repo\\Studio\\Salon" -o test_output
+    python "C:\\BraidTechnologies\\Repo\\Studio\Salon\\src\\repo_to_text_old.py" --cfg config.yaml --repo_path . -o test_output
 """
 
 import argparse
