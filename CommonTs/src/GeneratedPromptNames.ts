ac64124b80dec92d944aa23ead9e6fb718f63c3b
Jon Verrier - Founder <jon@braidtech.ai>
2025-02-26 21:15:33 +0000

    Was missing a GUID in prior version.

--- CommonTs/src/GeneratedPromptNames.ts
+++ CommonTs/src/GeneratedPromptNames.ts
@@ -10,4 +10,4 @@ export const articleSummariserPromptId: string = "716c667d-9074-4d9a-8335-319421
 export const developerQuestionGeneratorPromptId: string = "72213d46-9e6d-4604-a6bd-32d8e2f594f3";
 export const articleClassifierPromptId: string = "af0c1ede-3e3a-4fbe-a361-bb8b4fc6d8c9";
 export const themeFinderPromptId: string = "3a34093d-95ea-4d46-8cf9-8f2f784d09da";
-export const testForSummmariseFailurePromptId: string = "";
+export const testForSummmariseFailurePromptId: string = "cbfc5d7b-9404-451e-aec5-585a717d56a6";
