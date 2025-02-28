9b62fa3a04bd052c70c05023a1372103938d1a05
pratham-kuril <pratham@braidtech.ai>
2025-02-28 16:57:58 +0000

    Update factory.py

--- Salon/src/directory_processor/factory.py
+++ Salon/src/directory_processor/factory.py
@@ -19,7 +19,7 @@ def getProcessorsRepoToText(model_type: str, max_words: int, output_dir: Path) -
     """
     processors = [
         ReadmeGenerator(model_type=model_type, priority=1),
-        C4Generator(model_type=model_type, priority=2),
+        # C4Generator(model_type=model_type, priority=2),
         CodeAggregator(max_words=max_words, output_dir=output_dir, priority=3)
     ]
     return sorted(processors, key=lambda x: x.priority)
@@ -30,7 +30,7 @@ def getProcessorsRepoToC4(model_type: str) -> List[DirectoryProcessor]:
     Return a list of DirectoryProcessor instances for the 'repo_to_c4' workflow.
     """
     processors = [
-        ReadmeGenerator(model_type=model_type, priority=1),
+        # ReadmeGenerator(model_type=model_type, priority=1),
         C4Generator(model_type=model_type, priority=2)
     ]
     return sorted(processors, key=lambda x: x.priority)
